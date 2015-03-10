11667ebf4307294e771250cdb5dd1f10d78e5f6b
PanaEk Warawit <p.warawit@gmail.com>
2015-03-10 13:53:00 +0700

    modify install_02.sh

--- install/install_02.sh
+++ install/install_02.sh
@@ -10,4 +10,16 @@ systemctl start postgresql-9.4
 firewall-cmd --permanent --add-port=5432/tcp
 firewall-cmd --reload
 
+# Modifying pg_hba.conf
+cp /var/lib/pgsql/9.4/data/pg_hba.conf /var/lib/pgsql/9.4/data/pg_hba.conf.orig
+sed -i "/^host/s/ident/md5/g" /var/lib/pgsql/9.4/data/pg_hba.conf
+
+# Modifying postgresql.conf -- allow tcp/ip connection
+cp /var/lib/pgsql/9.4/data/postgresql.conf /var/lib/pgsql/9.4/data/postgresql.conf.orig
+sed -i "s/max_connections = 100/listen_addresses = '*'\nport = 5432\nmax_connections = 100/g" /var/lib/pgsql/9.4/data/postgresql.conf
+
+# Restart postgresql server
+systemctl restart postgresq-9.4
+
+
 
