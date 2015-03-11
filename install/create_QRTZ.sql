db3c7efa0577edc8f2831446353502bcbc180f22
PanaEk Warawit <p.warawit@gmail.com>
2015-03-11 12:07:46 +0700

    bug fix

---
+++ install/create_QRTZ.sql
@@ -0,0 +1,20 @@
+-- This script is a hack, work around to issue of QuartzSystemListener.ERROR_0007_SEQERROR
+-- It is basically create a fake table "QRTZ" to fool the script that start listener in quartz database
+
+--Begin Connect--
+\connect quartz pentaho_user
+
+begin;
+
+drop table if exists QRTZ;
+
+CREATE TABLE QRTZ
+  (
+    NAME  VARCHAR(200) NOT NULL,
+    PRIMARY KEY (NAME)
+);
+
+ALTER TABLE QRTZ OWNER TO pentaho_user;
+
+commit;
+--End Connect--
--- install/install_06.sh
+++ install/install_06.sh
@@ -4,4 +4,5 @@
 su - postgres -c "psql -a -f /opt/pentaho/biserver-ce/data/postgresql/create_quartz_postgresql.sql"
 su - postgres -c "psql -a -f /opt/pentaho/biserver-ce/data/postgresql/create_repository_postgresql.sql"
 su - postgres -c "psql -a -f /opt/pentaho/biserver-ce/data/postgresql/create_jcr_postgresql.sql"
+su - postgres -c "psql -a -f /opt/fidm/install/create_QRTZ.sql"
 
