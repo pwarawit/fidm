a392e605f569e91dd6505b345f986699ea190166
PanaEk Warawit <p.warawit@gmail.com>
2015-03-15 01:37:53 +0700

    bug fix load_gl_line.sh

--- data/to_load/load_gl_line_01.sh
+++ data/to_load/load_gl_line_01.sh
@@ -6,13 +6,13 @@ filename = 'gl_line_01.txt'
 
 file -bi gl_line_01.txt
 echo "  cutting the head and tail of the file "
-sed -n "9,147 p" gl_line_01.txt > tmpfile.txt
+sed -n "9,2040 p" gl_line_01.txt > tmpfile.txt
 echo "  Pre-processing - remove first |, last |, and double-quote" 
 
 sed -i "s/^|//g" tmpfile.txt
 sed -i "s/|$//g" tmpfile.txt
 sed -i "s/\"//g" tmpfile.txt
 
-psql -d fidm  -c "\copy tmp_gl_line( symbol,assignment,document_number, business_area,document_type,posting_key,amount_in_local_currency,local_currency,tax_code,clearning_document,profit_center,segment,text,document_header_text,entry_date,parked_by,user_name,activity_code,benefit_county,cf_leadership,donor,donor_line,gender_rights,monitor_evaluate,outcome,staff,theme) FROM 'tmpfile.txt' WITH CSV DELIMITER '|';"
+psql -d fidm  -c "\copy tmp_gl_line( symbol,assignment,document_number, business_area,document_type,document_date,posting_key,amount_in_local_currency,local_currency,tax_code,clearing_document,profit_center,segment,text,document_header_text,entry_date,parked_by,user_name,activity_code,benefit_county,cf_leadership,donor,donor_line,gender_rights,monitor_evaluate,outcome,staff,theme) FROM 'tmpfile.txt' CSV DELIMITER '|';"
 
 
