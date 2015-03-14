6579c28e5bb5e51b9a2bd7d67002b1ea702bd171
PanaEk Warawit <p.warawit@gmail.com>
2015-03-15 01:06:56 +0700

    add load_gr_line.sh

---
+++ sh/create_tmp_gl.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# create_fidm_database.sh
+
+set -e
+set -u
+
+psql \
+    -X \
+    -f /opt/fidm/sql/create_tmp_gl.sql \
+    --echo-all
+    --set AUTOCOMMIT=off \
+    --set ON_ERROR_STOP=on
+
+psql_exit_status = $?
+
+if [ $psql_exit_status != 0 ]; then
+    echo "psql failed while trying to run this sql script" 1>&2
+    exit $psql_exit_status
+fi
+
+echo "sql script successful"
+exit 0
---
+++ sql/create_tmp_gl.sql
@@ -0,0 +1,41 @@
+--Begin Connect--
+\connect fidm fidm
+
+begin;
+
+DROP TABLE IF EXISTS tmp_gl_line;
+
+CREATE TABLE tmp_gl_line(
+    symbol VARCHAR(2), 
+    assignment VARCHAR(18),
+    document_number VARCHAR(10),
+    business_area   VARCHAR(4),
+    document_type   VARCHAR(3),
+    posting_key     VARCHAR(2),
+    amount_in_local_currency    VARCHAR(20),
+    local_currency  VARCHAR(5),
+    tax_code        VARCHAR(2),
+    clearning_document   VARCHAR(10),
+    profit_center   VARCHAR(10),
+    segment    VARCHAR(10),
+    text       VARCHAR(50),
+    document_header_text VARCHAR(25),
+    entry_date VARCHAR(10),
+    parked_by  VARCHAR(12),
+    user_name  VARCHAR(12),
+    activity_code VARCHAR(10),
+    benefit_county VARCHAR(4),
+    cf_leadership  VARCHAR(1),
+    donor          VARCHAR(5),
+    donor_line     VARCHAR(10),
+    gender_rights  VARCHAR(1),
+    monitor_evaluate VARCHAR(1),
+    outcome          VARCHAR(1),
+    staff            VARCHAR(9),
+    theme            VARCHAR(1)
+);
+
+ALTER TABLE tmp_gl_line OWNER TO fidm;
+
+commit;
+--End Connect--
