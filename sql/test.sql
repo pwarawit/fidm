e6a449d1896972592f727318aaab02e53a2e4c8b
pwarawit <p.warawit@gmail.com>
2015-08-06 12:02:13 +0700

    updated v_awp_budget view

--- sql/test.sql
+++ sql/test.sql
@@ -1,9 +1,18 @@
-drop view v_awp_prf_expense;
-drop view v_awp_rtg_expense;
-drop view v_awp_rcf_expense;
-drop view v_awp_prj_expense;
-drop view v_awp_prf_income;
-drop view v_awp_rtg_income;
-drop view v_awp_rcf_income;
-drop view v_awp_prj_income;
-
+insert into dt_awp
+	(time_period, order_number, currency
+    , total_budget0_ytd
+	, prf_expense_mth
+    , rcf_expense_mth
+	)
+	select 
+        a.time_period, a.order_number, a.currency
+        , sum(a.dist_budget_amt) as total_budget0_ytd 
+        , sum(b.value_mth) as prf_expense_mth
+        , sum(c.value_mth) as rcf_expense_mth
+	from 
+        v_awp_budget a
+        left outer join v_awp_prf_expense b on (a.time_period=b.time_period AND a.order_number=b.order_number AND a.currency=b.currency)
+        left outer join v_awp_rcf_expense c on (a.time_period=c.time_period AND a.order_number=c.order_number AND a.currency=c.currency)
+    where a.time_period=201501
+    group by a.time_period, a.order_number, a.currency
+    ;
---
+++ sql/test2.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE VIEW v_awp_budget AS 
+SELECT
+    201501 AS time_period
+    , substring(object_number, 3, length(object_number)) AS order_number
+    , trans_currency AS currency
+    , sum(dist_value_trans_currency) AS dist_budget_amt
+FROM 
+    sap_bpge
+WHERE 
+    value_type='42' 
+    AND budget_type='KBFC'
+GROUP BY 
+    --to_number('201500', '999999') 
+    --, 
+    substring(object_number, 3, length(object_number)) 
+    , trans_currency
+UNION ALL
+SELECT
+    201501 AS time_period
+    , substring(object_number, 3, length(object_number)) AS order_number
+    , trans_currency AS currency
+    , sum(dist_annual_value_trans_currency) AS dist_budget_amt
+FROM 
+    sap_bpja
+WHERE 
+    value_type='42' 
+    AND budget_type='KBFC'
+GROUP BY 
+    --fiscal_year
+    --, 
+    substring(object_number, 3, length(object_number)) 
+    , fiscal_year
+    , trans_currency
+ORDER BY 
+    time_period
+    , order_number
+;
